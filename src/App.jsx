eb59700a820c07bec2428ccb26c59cc7c8bafffd
unknown <niel.rav.15@gmail.com>
2026-03-10 15:45:22 -0700

    creating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Cart, Home, Login, ProductDetails, Profile, Register, Shop, Dashboard, ProductListAdmin} from './pages/importPages'
 import { Footer, Navbar, Sidebar, ProductCard, ProductSlider, Button } from './components/importComponents'
-import './App.scss'
+// import './App.scss'
 
 function App() 
 {
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,25 @@
+import './navbar.scss'
 
 const Navbar = () => 
 {
   return (
     <>
-      <h1>Navbar</h1>
+      <header className="header">
+        <div className="header-logo">
+          <a href="#">Logo</a>
+        </div>
+      </header>
+      <nav className="navbar">
+        <ul className="navbar-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">PS5</a></li>
+          <li><a href="#">PS4</a></li>
+          <li><a href="#">Nintendo Switch 2</a></li>
+          <li><a href="#">Nintendo Switch </a></li>
+          <li><a href="#">Xbox Series X</a></li>
+          <li><a href="#">Xbox One</a></li>
+        </ul>
+      </nav>
     </>
   )
 }
