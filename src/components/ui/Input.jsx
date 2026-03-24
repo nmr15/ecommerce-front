b671dd38d740bb959e76897ca21bb45c9cb624b1
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2026-03-24 08:55:47 -0700

    created Input component

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-const Input = () => 
+const Input = ({ type, name, value, onChange, for, label }) => 
 {
   return (
     <>
+      <label htmlFor={for}>{label}</label>
       <input 
-        type={text}
+        type={type}
+        name={name}
         value={value}
         onChange={onChange}
+        className="input"
       />
     </>
   )
